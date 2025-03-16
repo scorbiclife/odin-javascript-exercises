2c6afcb5cba446e891bd48e8f1473fd71756b53d
scorbiclife <scorbiclife+git@gmail.com>
2025-03-16 10:40:06 +0900

    feat: solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const chars = [...string];
+  chars.reverse();
+  return chars.join("");
 };
 
 // Do not edit below this line
