08c16916fc9c255bd5e60a9bed54cf58e2339737
scorbiclife <scorbiclife+git@gmail.com>
2025-03-16 16:20:43 +0900

    feat: solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+function round(number, decimalPlaces) {
+  return Math.round(number * 10 ** decimalPlaces) / 10 ** decimalPlaces;
+}
+
+const convertToCelsius = function (farenheit) {
+  const celcius = (5 / 9) * (farenheit - 32);
+  return round(celcius, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  return round(1.8 * celcius + 32, 1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
