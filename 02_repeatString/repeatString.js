ab2ed5a0a2efeb457b1c6a9b41f7ba97c1a1e407
scorbiclife <scorbiclife+git@gmail.com>
2025-03-16 10:31:31 +0900

    feat: solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  const chars = [];
+  for (let i = 0; i < times; ++i) {
+    for (const c of string) {
+      chars.push(c);
+    }
+  }
+  return chars.join("");
 };
 
 // Do not edit below this line
