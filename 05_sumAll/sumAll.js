5194c029b49ef35b41cce514660efdf60042a351
scorbiclife <scorbiclife+git@gmail.com>
2025-03-16 16:11:48 +0900

    feat: solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+function validInput(i) {
+  return typeof i === "number" && i >= 0 && Number.isInteger(i);
+}
 
+const sumAll = function (x, y) {
+  if (!(validInput(x) && validInput(y))) {
+    return "ERROR";
+  }
+  const [start, end] = x < y ? [x, y] : [y, x];
+  let sum = 0;
+  for (let i = start; i <= end; ++i) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
