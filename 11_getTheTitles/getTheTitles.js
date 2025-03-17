1893fb85e345c9ef378057c86cca6cb62bc21ff3
scorbiclife <scorbiclife+git@gmail.com>
2025-03-17 16:09:00 +0900

    feat: solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+function getTheTitle(book) {
+  return book.title;
+}
 
+const getTheTitles = function (books) {
+  return books.map(getTheTitle);
 };
 
 // Do not edit below this line
