dbbd74dc6bd6b50fb81ddf2d5856c1422760ee74
scorbiclife <scorbiclife+git@gmail.com>
2025-03-17 15:59:32 +0900

    feat: solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const isAlphanumeric = (c) =>
+    ("a" <= c && c <= "z") || ("0" <= c && c <= "9");
+  const charsThatCount = [...string.toLowerCase()].filter(isAlphanumeric);
+  let [i, j] = [0, charsThatCount.length - 1];
+  for (; i < j; ++i, --j) {
+    if (charsThatCount[i] !== charsThatCount[j]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
