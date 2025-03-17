1d43e6a351e65e6191455949251dda65f663f9ab
scorbiclife <scorbiclife+git@gmail.com>
2025-03-17 16:15:00 +0900

    feat: solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+function getAge(person) {
+  return (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth;
+}
 
+function getOlderPerson(p1, p2) {
+  return getAge(p1) > getAge(p2) ? p1 : p2;
+}
+const findTheOldest = function (ps) {
+  return ps.reduce(getOlderPerson);
 };
 
 // Do not edit below this line
