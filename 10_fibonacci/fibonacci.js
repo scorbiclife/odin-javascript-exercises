c31c82e304367edc27ecdb7d3423d930d299530e
scorbiclife <scorbiclife+git@gmail.com>
2025-03-17 16:07:20 +0900

    feat: solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+  const n = Number(input);
+  if (!Number.isInteger(n)) {
+    return "OOPS";
+  }
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  let [x, y] = [0, 1];
+  for (let i = 2; i <= n; ++i) {
+    [x, y] = [y, x + y];
+  }
+  return y;
 };
 
 // Do not edit below this line
